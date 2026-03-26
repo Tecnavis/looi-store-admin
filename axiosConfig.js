a6aff3839f721761980ae7f4d3948966104ab427
thabumon <52034689+thabumon@users.noreply.github.com>
2026-03-26 09:35:24 +0530

    Update axiosConfig.js

--- axiosConfig.js
+++ axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const BASE_URL =
   import.meta.env?.VITE_BASE_URL ||
-  'https://looi-store-server-izvs.onrender.com';
+  'https://looi-store-server-izvs.onrender.com/api';
 
 const instance = axios.create({
   baseURL: BASE_URL,
